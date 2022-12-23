e8191800595aebcc73d8bdf89257080cc63e5182
BloodTaint <Bloodtaint@yahoo.com>
2022-12-23 15:16:58 +0200

    Django project PetStagram

---
+++ petstagram/petstagram/common/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from petstagram.common.models import PhotoComment, PhotoLike
+
+
+@admin.register(PhotoComment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PhotoLike)
+class LikeAdmin(admin.ModelAdmin):
+    pass
